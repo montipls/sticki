274a882cb45cb3d1269c63577242ec21395a95e1
Monti <montikoszo@gmail.com>
2025-05-02 20:35:00 +0200

    fixed list_items.py

--- list_items.py
+++ list_items.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     
     with open("items.html", "w") as file:
         file.write('<html><head><link rel="stylesheet" href="styles.css"></head><body>')
-        file.write("<h1>Enemies</h1><ul>")
+        file.write("<h1>Items</h1><ul>")
         file.write('<a href="index.html" title="Home">Back (Home)</a><br><br><br>')
         for name, data in drop_links.items():
             href = data[0][2]
